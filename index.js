0d7da13a57ca544ec34e25611b2b0cfd2292a83e
NYCDOE <admin@RM325-S15.local>
2015-08-04 14:16:08 -0400

    fifth commit

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 /*
-Copyright (c) 2013 dissimulate at Codepen
-
+Copyright (c) 2013 Suffick at Codepen (http://codepen.io/suffick) and GitHub (https://github.com/suffick)
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,23 +17,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-document.getElementById('close').onmousedown = function(e) {
-  e.preventDefault();
-  document.getElementById('info').style.display = 'none';
-  return false;
-};
-
 // settings
 
-var physics_accuracy  = 3,
-    mouse_influence   = 20,
-    mouse_cut         = 5,
-    gravity           = 1200,
-    cloth_height      = 30,
-    cloth_width       = 50,
-    start_y           = 20,
-    spacing           = 7,
-    tear_distance     = 60;
+var physics_accuracy = 3,
+    mouse_influence = 20,
+    mouse_cut = 5,
+    gravity = 1200,
+    cloth_height = 30,
+    cloth_width = 50,
+    start_y = 20,
+    spacing = 7,
+    tear_distance = 60;
 
 
 window.requestAnimFrame =
@@ -65,15 +56,15 @@ var canvas,
 
 var Point = function (x, y) {
 
-    this.x      = x;
-    this.y      = y;
-    this.px     = x;
-    this.py     = y;
-    this.vx     = 0;
-    this.vy     = 0;
-    this.pin_x  = null;
-    this.pin_y  = null;
-    
+    this.x = x;
+    this.y = y;
+    this.px = x;
+    this.py = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.pin_x = null;
+    this.pin_y = null;
+
     this.constraints = [];
 };
 
@@ -112,7 +103,7 @@ Point.prototype.update = function (delta) {
 
 Point.prototype.draw = function () {
 
-    if (!this.constraints.length) return;
+    if (this.constraints.length <= 0) return;
 
     var i = this.constraints.length;
     while (i--) this.constraints[i].draw();
@@ -130,8 +121,23 @@ Point.prototype.resolve_constraints = function () {
     var i = this.constraints.length;
     while (i--) this.constraints[i].resolve();
 
-    this.x > boundsx ? this.x = 2 * boundsx - this.x : 1 > this.x && (this.x = 2 - this.x);
-    this.y < 1 ? this.y = 2 - this.y : this.y > boundsy && (this.y = 2 * boundsy - this.y);
+    if (this.x > boundsx) {
+
+        this.x = 2 * boundsx - this.x;
+        
+    } else if (this.x < 1) {
+
+        this.x = 2 - this.x;
+    }
+
+    if (this.y > boundsy) {
+
+        this.y = 2 * boundsy - this.y;
+        
+    } else if (this.y < 1) {
+
+        this.y = 2 - this.y;
+    }
 };
 
 Point.prototype.attach = function (point) {
@@ -141,9 +147,11 @@ Point.prototype.attach = function (point) {
     );
 };
 
-Point.prototype.remove_constraint = function (constraint) {
+Point.prototype.remove_constraint = function (lnk) {
 
-    this.constraints.splice(this.constraints.indexOf(constraint), 1);
+    var i = this.constraints.length;
+    while (i--)
+        if (this.constraints[i] == lnk) this.constraints.splice(i, 1);
 };
 
 Point.prototype.add_force = function (x, y) {
@@ -159,17 +167,17 @@ Point.prototype.pin = function (pinx, piny) {
 
 var Constraint = function (p1, p2) {
 
-    this.p1     = p1;
-    this.p2     = p2;
+    this.p1 = p1;
+    this.p2 = p2;
     this.length = spacing;
 };
 
 Constraint.prototype.resolve = function () {
 
-    var diff_x  = this.p1.x - this.p2.x,
-        diff_y  = this.p1.y - this.p2.y,
-        dist    = Math.sqrt(diff_x * diff_x + diff_y * diff_y),
-        diff    = (this.length - dist) / dist;
+    var diff_x = this.p1.x - this.p2.x,
+        diff_y = this.p1.y - this.p2.y,
+        dist = Math.sqrt(diff_x * diff_x + diff_y * diff_y),
+        diff = (this.length - dist) / dist;
 
     if (dist > tear_distance) this.p1.remove_constraint(this);
 
@@ -245,13 +253,13 @@ function update() {
 function start() {
 
     canvas.onmousedown = function (e) {
-        mouse.button  = e.which;
-        mouse.px      = mouse.x;
-        mouse.py      = mouse.y;
-        var rect      = canvas.getBoundingClientRect();
-        mouse.x       = e.clientX - rect.left,
-        mouse.y       = e.clientY - rect.top,
-        mouse.down    = true;
+        mouse.button = e.which;
+        mouse.px = mouse.x;
+        mouse.py = mouse.y;
+        var rect = canvas.getBoundingClientRect();
+        mouse.x = e.clientX - rect.left,
+        mouse.y = e.clientY - rect.top,
+        mouse.down = true;
         e.preventDefault();
     };
 
@@ -261,11 +269,11 @@ function start() {
     };
 
     canvas.onmousemove = function (e) {
-        mouse.px  = mouse.x;
-        mouse.py  = mouse.y;
-        var rect  = canvas.getBoundingClientRect();
-        mouse.x   = e.clientX - rect.left,
-        mouse.y   = e.clientY - rect.top,
+        mouse.px = mouse.x;
+        mouse.py = mouse.y;
+        var rect = canvas.getBoundingClientRect();
+        mouse.x = e.clientX - rect.left,
+        mouse.y = e.clientY - rect.top,
         e.preventDefault();
     };
 
@@ -277,18 +285,16 @@ function start() {
     boundsy = canvas.height - 1;
 
     ctx.strokeStyle = '#888';
-  
     cloth = new Cloth();
-  
     update();
 }
 
 window.onload = function () {
 
-    canvas  = document.getElementById('c');
-    ctx     = canvas.getContext('2d');
+    canvas = document.getElementById('c');
+    ctx = canvas.getContext('2d');
 
-    canvas.width  = 560;
+    canvas.width = 560;
     canvas.height = 350;
 
     start();
